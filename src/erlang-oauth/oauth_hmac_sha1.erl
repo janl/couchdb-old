e62724ecbfcc8ae5627aa689bc226231f0c367ed
Jan Lehnardt <jan@apache.org>
2010-01-07 17:41:33 +0100

    Revert "add debug"
    This reverts commit 39317c565b7bd20dc24d7e064edd26aa2a42056a.

--- src/erlang-oauth/oauth_hmac_sha1.erl
+++ src/erlang-oauth/oauth_hmac_sha1.erl
@@ -5,11 +5,7 @@
 
 signature(BaseString, CS, TS) ->
   Key = oauth_uri:calate("&", [CS, TS]),
-  io:format("Key: '~p'~n", [Key]),
   base64:encode_to_string(crypto:sha_mac(Key, BaseString)).
 
 verify(Signature, BaseString, CS, TS) ->
-  io:format("BaseString: '~p'~n", [BaseString]),
-  ComputedSignature = signature(BaseString, CS, TS),
-  io:format("ComputedSignature: '~p'~n", [ComputedSignature]),
-  Signature =:= ComputedSignature.
+  Signature =:= signature(BaseString, CS, TS).
